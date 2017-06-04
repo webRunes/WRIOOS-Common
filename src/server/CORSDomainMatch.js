f730baa93aa876960d5e52fa88d490d607494670
Michael Bilenko <denso.ffff@gmail.com>
2017-06-04 20:26:17 +0300

    Rm debug text

--- src/server/CORSDomainMatch.js
+++ src/server/CORSDomainMatch.js
@@ -4,7 +4,7 @@
 const logger = require('winston');
 
 function CORSomainMatch(origin,domain) {
-    domain = domain.replace(/\./g,'\\.')+'(:3033)?$';console.log(domain);
+    domain = domain.replace(/\./g,'\\.')+'(:3033)?$';
     logger.log('silly',domain);
     return !!origin.match(new RegExp(domain,'m'));
 }
